5afc735a3d57c4fd2b8faeec34e610fe8e8c08bb
Tony Morris <tmorris@tmorris.net>
2014-11-24 11:32:25 +1000

    0.0.4

--- src/Data/Radian.hs
+++ src/Data/Radian.hs
@@ -2,75 +2,62 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Data.Radian(
-  Radian
-, radians
-, degrees
+  toRadians
+, fromRadians
 ) where
 
 import Control.Lens(Iso, iso, from)
-import Data.Eq(Eq)
-import Data.Ord(Ord)
-import Prelude(Show, Num((*)), Fractional((/)), Floating, Real, RealFrac, RealFloat, pi)
+import Prelude(Num((*)), Fractional((/)), Floating, pi)
 
 -- $setup
 -- >>> import Control.Lens((#))
 -- >>> import Prelude(Double)
 
-newtype Radian a =
-  Radian a
-  deriving (Eq, Ord, Show, Num, Fractional, Floating, Real, RealFrac, RealFloat)
-
--- | An isomorphism from radians to degrees.
+-- | An isomorphism between radians and degrees.
 --
--- >>> radians # (180 :: Double)
--- Radian 3.141592653589793
+-- >>> toRadians # (180 :: Double)
+-- 3.141592653589793
 --
--- >> radians # (90 :: Double)
--- Radian 1.5707963267948966
+-- >> toRadians # (90 :: Double)
+-- 1.5707963267948966
 --
--- >>> radians # (359 :: Double)
--- Radian 6.265732014659643
+-- >>> toRadians # (359 :: Double)
+-- 6.265732014659643
 --
--- >>> radians # (360 :: Double)
--- Radian 6.283185307179586
+-- >>> toRadians # (360 :: Double)
+-- 6.283185307179586
 --
--- >>> radians # (3600 :: Double)
--- Radian 62.83185307179586
+-- >>> toRadians # (3600 :: Double)
+-- 62.83185307179586
 --
--- >>> radians # (1 :: Double)
--- Radian 1.7453292519943295e-2
+-- >>> toRadians # (1 :: Double)
+-- 1.7453292519943295e-2
 --
--- >>> radians # ((-180) :: Double)
--- Radian (-3.141592653589793)
-radians ::
+-- >>> toRadians # ((-180) :: Double)
+-- -3.141592653589793
+toRadians ::
   (Floating a, Floating b) =>
-  Iso (Radian a) (Radian b) a b
-radians =
+  Iso a b a b
+toRadians =
   iso
-    (\(Radian a) -> a / pi * 180)
-    (\a -> Radian (a / 180 * pi))
+    (\a -> a / pi * 180)
+    (\a -> a / 180 * pi)   
 
--- | An isomorphism from degrees to radians.
+-- | An isomorphism between degrees and radians.
 --
--- >>> degrees # (3.14 :: Radian Double)
--- 179.90874767107852
---
--- >>> degrees # (1 :: Radian Double)
--- 57.29577951308232
---
--- >>> degrees # (10 :: Radian Double)
--- 572.9577951308232
---
--- >>> degrees # (0 :: Radian Double)
+-- >>> fromRadians # (0 :: Double)
 -- 0.0
 --
--- >>> degrees # (-1 :: Radian Double)
+-- >>> fromRadians # (1 :: Double)
+-- 57.29577951308232
+--
+-- >>> fromRadians # ((-1) :: Double)
 -- -57.29577951308232
 --
--- >>> degrees # (-3 :: Radian Double)
--- -171.88733853924697
-degrees ::
+-- >>> fromRadians # (3 :: Double)
+-- 171.88733853924697
+fromRadians ::
   (Floating a, Floating b) =>
-  Iso a b (Radian a) (Radian b)
-degrees =
-  from radians
+  Iso a b a b
+fromRadians =
+  from toRadians
